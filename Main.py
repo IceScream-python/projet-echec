b3b7561710f67448f7682c807fe38f558ebc0955
IceScream-python <79906543+IceScream-python@users.noreply.github.com>
2021-09-23 22:05:13 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,40 @@
 class piece:
-    def __init__(self,absisse,ordonnee,couleur,classe):
+    def __init__(self,absisse,ordonnee,couleur):
         self.x = absisse
         self.y = ordonnee
         self.couleur = couleur
-        self.classe = classe
 	
 
 class pion(piece):
+    def __init__(self,absisse,ordonnee,couleur):
+        self.classe = 'pion'
+        super().__init__(absisse,ordonnee,couleur)
+
     def __str__(self):
         return f"Pion, ({self.x},{self.y})"
+
     def avancer(self):
         if self.couleur == "blanc":
-            deplacement = 1
+            self.mouvement(0,1)
         else:
-            deplacement = -1
-        if plateau.contenu_case(self.x,self.y+deplacement)[0] == "empty":
-            plateau.plateau_jeu[self.y+deplacement-1][self.x-1] = (self, self.x, self.y+deplacement)
+            self.mouvement(0,-1)
+
+    def droite(self):
+        self.mouvement(1,0)
+
+    def gauche(self):
+        self.mouvement(-1,0)
+
+    def mouvement(self,deplacement_x, deplacement_y):
+        if plateau.contenu_case(self.x+deplacement_x,self.y+deplacement_y)[0] == "empty":
+            plateau.plateau_jeu[self.y+deplacement_y-1][self.x+deplacement_x-1] = (self, self.x+deplacement_x, self.y+deplacement_y)
             plateau.plateau_jeu[self.y-1][self.x-1] = ("empty",self.x,self.y)
-            self.y += deplacement
+            self.y += deplacement_y
+            self.x += deplacement_x
+            print(self)
+            print(plateau)
+        elif type(plateau.contenu_case(self.x+deplacement_x,self.y+deplacement_y)[0]) == pion and plateau.contenu_case(self.x+deplacement_x,self.y+deplacement_y)[0].couleur != self.couleur:
+            print('je peux manger')
         else:
             print("Je ne peux pas avancer")
 
@@ -28,17 +45,18 @@ class Plateau:
         self.coordonees = {}
         for pion in liste:
             self.coordonees[(pion.x,pion.y)] = pion
-        for x in range(1, size+1):
+        for y in range(1, size+1):
             self.plateau_jeu.append( list() )
-            for y in range(1, size+1):
+            for x in range(1, size+1):
                 try:
-                    self.plateau_jeu[x-1].append( (self.coordonees[(x,y)],x,y) )
+                    self.plateau_jeu[y-1].append( (self.coordonees[(x,y)],x,y) )
                 except:
-                    self.plateau_jeu[x-1].append( ("empty",x,y) )
+                    self.plateau_jeu[y-1].append( ("empty",x,y) )
+
     def __str__(self):
         string = ""
         for y in self.plateau_jeu:
-            string += "\n" 
+            string += "\n"
             for x in y:
                 string += "|"
                 try:
@@ -46,12 +64,17 @@ class Plateau:
                 except:
                     string += x[0]
         return string
+
     def contenu_case(self,x,y):
         try:
-            return self.plateau_jeu[x-1][y-1]
+            assert x!=0 and y!=0
+            return self.plateau_jeu[y-1][x-1]
         except:
             return "out"
 
-p1 = pion(1,1,'blanc','pion')
-p2 = pion(4,4,'noir','pion')
-plateau = Plateau([p1,p2],4)
+p1 = pion(1,1,'blanc')
+p2 = pion(1,2,'noir')
+p3 = pion(4,4,'noir')
+p4 = pion(4,3,'noir')
+plateau = Plateau([p1,p2,p3,p4],4)
+print(plateau)
